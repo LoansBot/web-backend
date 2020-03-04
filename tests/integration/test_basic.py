913e8f34b7402b1a2f9c904e2fb48b7e17064f8d
Timothy Moore <mtimothy984@gmail.com>
2020-03-03 18:35:17 -0800

    Add basic 200 response tests

--- tests/integration/test_basic.py
+++ tests/integration/test_basic.py
@@ -1,1 +1,22 @@
 """A sample test that is expected to be removed later"""
+import unittest
+import requests
+import os
+
+
+HOST = os.environ['TEST_WEB_HOST']
+
+class BasicResponseTests(unittest.TestCase):
+    def test_root_gives_200(self):
+        r = requests.get(HOST)
+        r.raise_for_status()
+        self.assertEquals(r.status_code, 200)
+
+    def test_log_gives_200(self):
+        r = requests.get(HOST + '/test_log')
+        r.raise_for_status()
+        self.assertEquals(r.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
