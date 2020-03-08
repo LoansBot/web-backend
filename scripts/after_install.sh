5d501974f6e1dfa47ab534e177ac66d36a02a61c
Timothy Moore <mtimothy984@gmail.com>
2020-03-08 16:28:13 -0700

    Fix not installing logging requirements

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sudo python3 -m pip install -r /webapps/lbapi/requirements.txt
+sudo python3 -m pip install -r /webapps/lbapi/logging-requirements.txt
 echo "Starting up supervisor"
 source /home/ec2-user/secrets.sh
 cd /webapps/lbapi/src
