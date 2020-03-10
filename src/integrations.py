7cc645bca7176e66a1dd1e3e1ee7dbb1f315181c
Timothy Moore <mtimothy984@gmail.com>
2020-03-09 19:02:44 -0700

    Don't reuse connections across requests

--- src/integrations.py
+++ src/integrations.py
@@ -1,26 +1,30 @@
-"""Initializes the logger"""
-from lblogging import Logger, Level
-import os
+"""Contains utility functions to connect with any other services, such as the
+database, while processing a request."""
+from lblogging import Logger
 from pymemcache.client import base as membase
 import psycopg2
 import pika
+import os
 
 
-def _init_logger(database):
-    logger = Logger(os.environ['APPNAME'], 'logging.py', database)
+def database():
+    return psycopg2.connect('')
+
+
+def logger():
+    conn = database()
+    logger = Logger(os.environ['APPNAME'], 'integrations.py', database)
     logger.prepare()
-    logger.print(Level.TRACE, 'Initialized')
-    logger.connection.commit()
     return logger
 
 
-def _init_memcached():
+def memcached():
     memcache_host = os.environ['MEMCACHED_HOST']
     memcache_port = int(os.environ['MEMCACHED_PORT'])
     return membase.Client((memcache_host, memcache_port))
 
 
-def _init_amqp():
+def amqp():
     parameters = pika.ConnectionParameters(
         os.environ['AMQP_HOST'],
         int(os.environ['AMQP_PORT']),
@@ -29,17 +33,7 @@ def _init_amqp():
             os.environ['AMQP_USERNAME'], os.environ['AMQP_PASSWORD']
         )
     )
-    return pika.BlockingConnection(parameters)
-
-
-def _init_amqp_channel(amqp):
+    amqp = pika.BlockingConnection(parameters)
     channel = amqp.channel()
     channel.confirm_delivery()
-    return channel
-
-
-DATABASE = psycopg2.connect('')
-LOGGER = _init_logger(DATABASE)
-MEMCACHED = _init_memcached()
-AMQP = _init_amqp()
-AMQP_CHANNEL = _init_amqp_channel(AMQP)
+    return amqp, channel
