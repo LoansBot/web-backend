95b62b0b867e9071a63e05fb98b5aacb06cc31da
Timothy Moore <mtimothy984@gmail.com>
2020-03-09 19:10:34 -0700

    Don't reuse connections (#3)

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI, Response, status
 from lblogging import Level
-from constants import LOGGER, MEMCACHED as cache, AMQP_CHANNEL as channel
+import integrations
 import json
 import secrets
 
 
 app = FastAPI()  # noqa
-logger = LOGGER.with_iden('main.py')  # noqa
 
 
 @app.get('/')
@@ -16,21 +15,28 @@ def root():
 
 @app.get('/test_log')
 def test_log():
+    logger = integrations.logger().with_iden('main.py')
     logger.print(Level.TRACE, 'test_log')
     logger.connection.commit()
+    logger.connection.close()
     return Response(status_code=status.HTTP_200_OK)
 
 
 @app.get('/test_cache')
 def test_cache():
+    cache = integrations.memcached()
     resp = json.dumps({'message': 'how you doing'})
     cache.set('test', resp.encode('utf-8'))
-    return json.loads(cache.get('test').decode('utf-8'))
+    res = json.loads(cache.get('test').decode('utf-8'))
+    cache.close()
+    return res
 
 
 @app.get('/test_amqp')
 def test_amqp():
+    amqp, channel = integrations.amqp()
     channel.queue_declare(queue='hello')
+
     pub_body = secrets.token_urlsafe(16)
     channel.basic_publish(
         exchange='',
@@ -40,18 +46,25 @@ def test_amqp():
     )
     for method_frame, properties, body_bytes in channel.consume('hello', inactivity_timeout=1):
         if method_frame is None:
+            logger = integrations.logger()
             logger.print(Level.WARN, 'test_amqp reached inactivity timeout')
             logger.connection.commit()
             channel.cancel()
+            amqp.close()
+            logger.connection.close()
             return Response(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
         channel.basic_ack(method_frame.delivery_tag)
         con_body = body_bytes.decode('utf-8')
         if con_body != pub_body:
+            logger = integrations.logger()
             logger.print(
                 Level.WARN,
                 'test_amqp expected response {} but got {}',
                 pub_body, con_body
             )
             logger.connection.commit()
-        channel.cancel()
-        return Response(status_code=status.HTTP_200_OK)
+            logger.close()
+        else:
+            channel.cancel()
+            amqp.close()
+            return Response(status_code=status.HTTP_200_OK)
