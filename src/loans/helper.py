2c9963d6ac06fc7f168e01437b99f17c64664c88
Timothy Moore <mtimothy984@gmail.com>
2020-06-02 07:35:07 -0700

    Fix bad creation links

--- src/loans/helper.py
+++ src/loans/helper.py
@@ -235,7 +235,7 @@ def get_loan_events(itgs, loan_id, perms):
                     None
                     if creation_type != 0
                     else
-                    f'https://reddit.com/comments/{parent_fullname}/redditloans/{comment_fullname}'
+                    f'https://reddit.com/comments/{parent_fullname[3:]}/redditloans/{comment_fullname[3:]}'
                 )
             )
         )
