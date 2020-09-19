f27c84a68a1d1859a3ee615877ca7274c53024a6
Timothy Moore <mtimothy984@gmail.com>
2020-09-19 08:05:04 -0700

    Add legacy get_csv_dump.php endpoint (#44)
    * Add legacy get_dump_csv.php redirect
    
    * Fix confusing comment

---
+++ src/legacy/loans/get_dump_csv_php.py
@@ -0,0 +1,43 @@
+"""This handles the deprecated endpoint /api/get_creation_info.php. See
++get_creation_info+ for details.
+"""
+from fastapi import APIRouter, Request
+from fastapi.responses import Response, RedirectResponse
+from legacy.helper import find_bearer_token, try_handle_deprecated_call
+import users.helper
+from lbshared.lazy_integrations import LazyIntegrations as LazyItgs
+
+
+SLUG = 'get_dump_csv'
+"""The slug for this legacy endpoint"""
+
+router = APIRouter()
+
+
+@router.get(
+    '/get_dump_csv.php',
+    responses={
+        307: {'description': 'Success'}
+    }
+)
+def get_dump_csv(request: Request):
+    """Redirects to /api/loans/loans.csv with the bearer token as a query
+    parameter.
+    """
+    with LazyItgs() as itgs:
+        auth = find_bearer_token(request)
+        user_id, _, _ = users.helper.get_permissions_from_header(itgs, auth, [])
+        resp = try_handle_deprecated_call(itgs, request, SLUG, user_id=user_id)
+
+        if resp is not None:
+            return resp
+
+        if auth is None:
+            return Response(status_code=401)
+
+        if user_id is None:
+            return Response(status_code=403)
+
+        return RedirectResponse(
+            url=f'/api/loans/loans.csv?alt_authorization={auth.split(" ", 1)[1]}'
+        )
