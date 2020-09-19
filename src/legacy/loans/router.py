65f6257f82199214324cda4b90e9759869e4377a
Timothy Moore <mtimothy984@gmail.com>
2020-09-19 08:00:07 -0700

    Add legacy get_dump_csv.php redirect

--- src/legacy/loans/router.py
+++ src/legacy/loans/router.py
@@ -1,7 +1,9 @@
 """The router for loans-related legacy endpoints"""
 from fastapi import APIRouter
 import legacy.loans.get_creation_info_php
+import legacy.loans.get_dump_csv_php
 
 
 router = APIRouter()
 router.include_router(legacy.loans.get_creation_info_php.router)
+router.include_router(legacy.loans.get_dump_csv_php.router)
