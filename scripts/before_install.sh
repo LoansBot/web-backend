523fb420ac53fcb55fac998053a3d6ceccdf3e70
Timothy Moore <mtimothy984@gmail.com>
2021-06-01 19:55:30 -0700

    Fix installation of psycopg2

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 sudo yum -y install make glibc-devel gcc patch perl-core zlib-devel postgresql-devel
 sudo yum -y install libffi libffi-devel
-if [ ! -f /usr/local/src/openssl-1.1.1g.tar.gz ]; then
+if [ ! -f /usr/local/src/openssl-1.1.1k.tar.gz ] || [ ! ls -ltra /usr/bin/python3 | grep -oE "\-> python3\.9" ]; then
     # We need openssl >= 1.1 for scrypt, and we need to build python with it
     # available. We also update python here to latest since we're building
     # from source anyway
     sudo yum remove python3 -y
 
     cd /usr/local/src
-    sudo wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz
-    sudo tar -xf openssl-1.1.1g.tar.gz
-    cd openssl-1.1.1g
+    sudo wget https://www.openssl.org/source/openssl-1.1.1k.tar.gz
+    sudo tar -xf openssl-1.1.1k.tar.gz
+    cd openssl-1.1.1k
     sudo ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared zlib
     sudo make
     sudo make install
-    sudo bash -c 'echo "/usr/local/ssl/lib" > "/etc/ld.so.conf.d/openssl-1.1.1g.conf"'
+    sudo bash -c 'echo "/usr/local/ssl/lib" > "/etc/ld.so.conf.d/openssl-1.1.1k.conf"'
     sudo ldconfig -v
     if [ -f /usr/bin/openssl ]; then
         sudo mv /usr/bin/openssl /usr/bin/openssl.BACKUP
@@ -22,13 +22,13 @@ if [ ! -f /usr/local/src/openssl-1.1.1g.tar.gz ]; then
     sudo cp -R /usr/local/ssl/bin /usr/
 
     cd /usr/local/src
-    sudo wget https://www.python.org/ftp/python/3.8.3/Python-3.8.3.tgz
-    sudo tar -xf Python-3.8.3.tgz
-    cd Python-3.8.3
-    sudo ./configure --prefix=/usr/local/python3.8 --enable-optimizations
+    sudo wget https://www.python.org/ftp/python/3.9.5/Python-3.9.5.tgz
+    sudo tar -xf Python-3.9.5.tgz
+    cd Python-3.9.5
+    sudo ./configure --prefix=/usr/local/python3.9 --enable-optimizations
     sudo make
     sudo make install
-    sudo ln -s /usr/local/python3.8/bin/python3.8 /usr/bin/python3
+    sudo ln -s /usr/local/python3.9/bin/python3.9 /usr/bin/python3 python3-devel
 fi
 sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install supervisor
